caeb0124d571b09b991009cec80edc62baa38041
eng-ronyislam <eng.ronyislam@gmail.com>
2025-07-28 23:34:57 +0600

    solved the first problem

---
+++ task1.js
@@ -0,0 +1,9 @@
+// Input
+const yourTaka = 1000;   // Money you have
+const totalCost = 700;   // Cost of oranges + apples
+
+// Calculation
+const returnMoney = yourTaka - totalCost;
+
+// Output
+console.log(returnMoney);
